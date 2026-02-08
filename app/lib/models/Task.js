89b33ea781ba345851a2b98c9ce65a16859d9118
kanika soni <ks1982006@gmail.com>
2026-02-08 17:18:26 +0530

    Create Task.js

---
+++ app/lib/models/Task.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.Task ||
+  mongoose.model("Task", TaskSchema);
