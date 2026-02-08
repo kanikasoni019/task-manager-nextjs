0a48468c2d09e7ef971b72ac69c4149954117944
kanika soni <ks1982006@gmail.com>
2026-02-08 18:32:15 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "@/app/lib/db";
-import Task from "@/app/models/Task";
+import { connectDB } from "../../lib/db";
+import Task from "../../models/Task";
 
 export async function GET() {
   try {
